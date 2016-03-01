1bc0ed8482e8128a57bbbce45048ecdd66637044
Gary Pang <gary.pang@gmail.com>
2016-03-01 03:00:54 -0500

    Add  gulp-ng-annotate
    Add AngularJS minifier with Gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@ var gulp = require('gulp'),
 	less = require('gulp-less'),
 	minifyCSS = require('gulp-minify-css'),
 	rename = require('gulp-rename'),
-	jshint = require('gulp-jshint');
+	jshint = require('gulp-jshint'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	ngAnnotate = require('gulp-ng-annotate');
 
 // define a task called css
 gulp.task('css', function() {
@@ -21,4 +24,25 @@ gulp.task('js', function() {
 	return gulp.src(['server.js', 'public.app/*.js', 'public/app/**/*.js'])
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'));
+});
+
+// task to lint, minify and concat frontend files
+gulp.task('scripts', function() {
+	return gulp.src(['public/app/*.js', 'public/app/**/*.js'])
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(concat('all.js'))
+		.pipe(uglify)
+		.pipe(gulp.dest('public/dist'));
+});
+
+// task to lint, minify and concat frontend angular files
+gulp.task('angular', function() {
+	return gulp.src(['public/app/*.js', 'public/app/**/*.js'])
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(ngAnnotate())
+		.pipe(concat('app.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('public/dist'));
 });
